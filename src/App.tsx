1590eabd4a76cb85cacf9c7e84e3a2d8fd504106
loup topalian <loup.topalian@gmail.com>
2023-08-07 22:06:23 +0200

    chore: Add github link

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Badge, Box, Flex, Space, Title } from "@mantine/core";
+import { ActionIcon, Badge, Box, Flex, Space, Title } from "@mantine/core";
 import { useEffect } from "react";
 import { TransformComponent, TransformWrapper } from "react-zoom-pan-pinch";
 import { Header } from "./components/Header";
@@ -6,6 +6,7 @@ import { MetadataCard } from "./components/MetadataCard";
 import { Option } from "./components/Option";
 import { state$ } from "./store";
 import { cleanAllUnusedAssets } from "./utils";
+import { IconBrandGithub, IconBrandGithubFilled } from "@tabler/icons-react";
 
 function App() {
   const initialOption = state$.state.initialOption.use();
@@ -15,56 +16,69 @@ function App() {
   }, [initialOption]);
 
   return (
-    <TransformWrapper
-      doubleClick={{ disabled: true }}
-      panning={{ excluded: ["input", "textarea"] }}
-      centerOnInit={false}
-      minScale={0.5}
-      onZoomStop={({ state: { scale } }) => {
-        state$.ui.scale.set(scale);
-      }}
-    >
-      <TransformComponent
-        wrapperStyle={{
-          height: "100vh",
-          width: "100vw",
-          cursor: "grab",
-        }}
-        contentStyle={{
-          background: "white",
-          backgroundImage: "radial-gradient(#bbb 1px, transparent 0)",
-          backgroundSize: "40px 40px",
-          backgroundPosition: "-19px -19px",
-          minHeight: "100vh",
-          minWidth: "100vw",
-          padding: "100px",
-          boxSizing: "border-box",
+    <>
+      <TransformWrapper
+        doubleClick={{ disabled: true }}
+        panning={{ excluded: ["input", "textarea"] }}
+        centerOnInit={false}
+        minScale={0.5}
+        onZoomStop={({ state: { scale } }) => {
+          state$.ui.scale.set(scale);
         }}
       >
-        <Box id="arrrow-frame" pos="relative">
-          <Title size={70}>
-            Lunii Admin Builder
-            <Badge color="lime">{__COMMIT_HASH__ || "xxxxx"} Alpha</Badge>
-          </Title>
+        <TransformComponent
+          wrapperStyle={{
+            height: "100vh",
+            width: "100vw",
+            cursor: "grab",
+          }}
+          contentStyle={{
+            background: "white",
+            backgroundImage: "radial-gradient(#bbb 1px, transparent 0)",
+            backgroundSize: "40px 40px",
+            backgroundPosition: "-19px -19px",
+            minHeight: "100vh",
+            minWidth: "100vw",
+            padding: "100px",
+            boxSizing: "border-box",
+          }}
+        >
+          <Box id="arrrow-frame" pos="relative">
+            <Title size={70}>
+              Lunii Admin Builder
+              <Badge color="lime">{__COMMIT_HASH__ || "xxxxx"} Alpha</Badge>
+            </Title>
 
-          <Header />
-          <Space h={50} />
-          <Flex>
-            <Box mr={50}>
-              <MetadataCard />
-            </Box>
-            <Option
-              option={initialOption}
-              id="option-main"
-              onUpdate={async (option) => {
-                state$.state.initialOption.set(option);
-                cleanAllUnusedAssets(option);
-              }}
-            />
-          </Flex>
-        </Box>
-      </TransformComponent>
-    </TransformWrapper>
+            <Header />
+            <Space h={50} />
+            <Flex>
+              <Box mr={50}>
+                <MetadataCard />
+              </Box>
+              <Option
+                option={initialOption}
+                id="option-main"
+                onUpdate={async (option) => {
+                  state$.state.initialOption.set(option);
+                  cleanAllUnusedAssets(option);
+                }}
+              />
+            </Flex>
+          </Box>
+        </TransformComponent>
+      </TransformWrapper>
+
+      <ActionIcon
+        pos="fixed"
+        top={10}
+        right={10}
+        component="a"
+        href="https://github.com/olup/lunii-admin-builder"
+        target="_blank"
+      >
+        <IconBrandGithubFilled />
+      </ActionIcon>
+    </>
   );
 }
 
